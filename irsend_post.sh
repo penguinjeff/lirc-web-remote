748ed67d5c16370fda3caf43676f485133d93abf
penguinjeff <jeff.sadowski@gmail.com>
2025-08-17 22:54:01 -0600

    removed child processes

--- irsend_post.sh
+++ irsend_post.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+result=''
 function replace()
 {
- local out=$1
+ result=$1
  shift
  while [ -n "$1" ];do
-  out=${out//$1/$2}
+  result=${result//$1/$2}
   shift 2
  done
- printf '%s\n' "$out"
 }
 
 bad()
@@ -24,13 +24,15 @@ while [ -n "$prev" ] || [ -n "$line" ];do
 prev=$line
 read -t .01 line
 #echo "$line"
-[ "$prev" = "$(echo -e "\r")" ] && start=""
+[ "$prev" = $'\r' ] && start=""
 [ -z "$start" ] && [ -n "$line" ] && accum+="$line"
 done
 #sed 's/[^A-Za-z0-9\_\.\-\+\=\&\{\}\%\[\] ]//g'
-steralized=$(replace "$accum" '[^A-Za-z0-9_.\-+=&\{\}%\[\] :,\"]' '' %5B \[ %5D \] %22 \")
+replace "$accum" '[^A-Za-z0-9_.\-+=&\{\}%\[\] :,\"]' '' %5B \[ %5D \] %22 \"
+steralized=$result
 printf "HTTP/1.1 200 OK\nContent-Type: application/json\n\n"
 #echo -e "$accum"
+#echo -e "$result"
 [ "${steralized:0:1}" != '{' ] && bad "missing { at beginning"
 [ "${steralized:0-1}" != '}' ] && bad "missing } at end"
 unwrapped="${steralized:1:$((${#steralized}-2))}"
