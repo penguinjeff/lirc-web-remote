890e299a29ecbae2530013daeea0bbd19d1d5186
penguinjeff <jeff.sadowski@gmail.com>
2025-10-07 23:50:31 -0600

    added some major functionality

--- js/0utils.js
+++ js/0utils.js
@@ -12,14 +12,93 @@ function index(list)
  return myobject;
 }
 
+
+function optionsOptions(json,onchange,noneOptions,item)
+{
+ if(!item){item=event.target;}
+ if(item.parentNode.childNodes.length>1)
+ {item.parentNode.removeChild(item.parentNode.lastChild);
+ }
+ if(item.value!='')
+ {var keys=Object.keys(json);
+  item.parentNode.appendChild(createElement({'options':json[keys[item.value]],'onchange':onchange,'noneOptions':noneOptions}));
+ }
+ if(onchange)
+ {setTimeout(onchange,0);
+ }
+}
+
+function relativeFor(relation)
+{// relation is a string of the following represented letters
+ // b before/previous
+ // n next
+ // p parent
+ // f first
+ // l last
+ 
+ var item=event.target;
+ var relativeitem=item;
+ var x=0;
+ for(x=0;x<relation.length;x++)
+ {switch(relation[x])
+  {case 'b': relativeitem=relativeitem.previousElementSibling;	break;
+   case 'n': relativeitem=relativeitem.nextElementSibling;		break;
+   case 'p': relativeitem=relativeitem.parentElement;			break;
+   case 'f': relativeitem=relativeitem.firstChild;				break;
+   case 'l': relativeitem=relativeitem.lastChild;				break;
+ }}
+ console.log("TagName:"+relativeitem.tagName);
+ console.log("name:"+relativeitem.getAttribute('name'));
+ if(relativeitem.type)
+ {
+  console.log("type:"+relativeitem.type);
+  relativeitem.click();
+  if(!relativeitem.checked){relativeitem.select();}
+}}
+
+
 //I wanted a way to pass more into a createElement to simplify my code
 //since I am using class names because html do no provide searchbyname on subelements
 //I add name to classname 
 function createElement(items)
-{
+{var onchange=''
+ if(items[onchange]){onchange=items[onchange];}
  var item;
- if(items['options'])
+ var keytags=new Set(['optionsOptions','options','tag','noneOptions','namevalues']);
+ if(onchange==''){keytags.add('onchange');}
+ if(items['optionsOptions'])
+ {item=document.createElement('div');
+  subitem=document.createElement('select');
+  keytags.add('onchange');
+  if(items['noneOptions'])
+  {var option=document.createElement('option');
+   option.text="None"
+   option.value='';
+   subitem.appendChild(option);
+  }
+  var keys=Object.keys(items['optionsOptions']);
+  for(var x=0;x<keys.length;x++)
+  {var option=document.createElement('option');
+   if(items['namevalues']){option.value=keys[x];}
+   else{option.value=x;}
+   option.text=keys[x];
+   subitem.appendChild(option);
+  }
+  subitem.setAttribute('onchange','optionsOptions('+JSON.stringify(items['optionsOptions'])+',\''+onchange+'\','+items['noneOptions']+')');
+  item.appendChild(subitem);
+  if(!items['noneOptions'])
+  {
+   optionsOptions(items['optionsOptions'],onchange,items['noneOptions'],item.firstChild);
+  }
+ }
+ else if(items['options'])
  {item=document.createElement('select');
+  if(items['noneOptions'])
+  {var option=document.createElement('option');
+   option.text="None"
+   option.value='';
+   item.appendChild(option);
+  }
   for(var x=0;x<items['options'].length;x++)
   {var option=document.createElement('option');
    if(items['namevalues']){option.value=items['options'][x];}
@@ -32,8 +111,8 @@ function createElement(items)
  }
  else
  {
-  alert('need either options or tag')
-  alert(JSON.stringify(items))
+  console.log('need either optionsOptions options or tag')
+  console.log(JSON.stringify(items))
   return null;
  }
  if(items['name'])
@@ -46,7 +125,7 @@ function createElement(items)
  }}
  var keys=Object.keys(items);
  for(var x=0;x<keys.length;x++)
- {if(keys[x]=='options'||keys[x]=='namevalues'||keys[x]=='tag'){continue;}
+ {if(keytags.has(keys[x])){continue;}
   if(keys[x]=='innerHTML')
   {item.innerHTML=items[keys[x]];
   }
@@ -58,4 +137,3 @@ function createElement(items)
  }}
  return item;
 }
-
