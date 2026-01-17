b0eb9dfab6ac400547dbfad0303674e1da7b4ca5
penguinjeff <jeff.sadowski@gmail.com>
2026-01-17 09:29:37 -0700

    added module properties

--- data/get_default_display.js
+++ data/get_default_display.js
@@ -2,7 +2,7 @@ function get_default_display()
 {
 	return	{
 	"modules":[[["remotename",0]],
-	           [["power",0]],[["catchall",0]],
+	           [["power",0]],[["catchall",0,{'width':5,'height':3}]],
 			   [["common",0]],
 			   [["colors",0]],
 			   [["volume",0],["directions",0],["channels",0]],
