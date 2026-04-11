dd105100859bfe98577563d249e71cf02a472f88
penguinjeff <jeff.sadowski@gmail.com>
2026-04-11 16:23:19 -0600

    Remove all ?ts=

---
+++ irsend_mult_test3.sh
@@ -0,0 +1,6 @@
+id=$(curl --no-progress-meter  'http://127.0.0.1/lirc-web-remote/irsend_mult.php' --data-raw 'mode=macro&json=[["Logitech_Samsung","KEY_VOLUMEDOWN",0,1]]')
+echo -e "$id"
+id=$(echo -e "$id"|head -n 1)
+id="${id#[\"}"
+id="${id%\"]}"
+curl --no-progress-meter 'http://127.0.0.1/lirc-web-remote/irsend_mult.php' --data-raw "mode=status&json=[\"$id\"]"
